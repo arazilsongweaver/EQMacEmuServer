3c24998d0a124cd1b1ec3b3335d8159e75d6a8a2
Arazil Songweaver <arazil@arazil.net>
2025-04-28 02:41:02 +0000

    Added current target support to #kick command.

--- zone/gm_commands/kick.cpp
+++ zone/gm_commands/kick.cpp
@@ -3,37 +3,44 @@
 extern WorldServer worldserver;
 
 void command_kick(Client *c, const Seperator *sep){
-	int arguments = sep->argnum;
-	if (!arguments) {
+	const char* name;
+	if (sep->arg[1])                // arg specified
+	{
+		name = sep->arg[1];
+	}
+	else if (c->GetTarget()) {               // have target
+		name = c->GetTarget()->GetName();
+	}
+	else
+	{
 		c->Message(Chat::White, "Usage: #kick [Character Name]");
 		return;
 	}
-	else {
-		std::string character_name = sep->arg[1];
-		auto client = entity_list.GetClientByName(character_name.c_str());
 
-		if (!worldserver.Connected())
-			c->Message(Chat::White, "The world server is currently disconnected.");
-		else {
-			auto pack = new ServerPacket(ServerOP_KickPlayer, sizeof(ServerKickPlayer_Struct));
-			ServerKickPlayer_Struct* skp = (ServerKickPlayer_Struct*)pack->pBuffer;
-			strcpy(skp->adminname, c->GetName());
-			strcpy(skp->name, character_name.c_str());
-			skp->adminrank = c->Admin();
-			worldserver.SendPacket(pack);
-			safe_delete(pack);
-		}
-		if (client) {
-			if (client->Admin() <= c->Admin()) {
-				client->Message_StringID(Chat::Red, StringID::BEEN_KICKED);
-				client->WorldKick();
-				c->Message(Chat::White, 
-					fmt::format(
-					"{} has been kicked from the server.",
-					character_name
-					).c_str()
-				);
-			}
+	auto client = entity_list.GetClientByName(name);
+
+	if (!worldserver.Connected())
+		c->Message(Chat::White, "The world server is currently disconnected.");
+	else {
+		auto pack = new ServerPacket(ServerOP_KickPlayer, sizeof(ServerKickPlayer_Struct));
+		ServerKickPlayer_Struct* skp = (ServerKickPlayer_Struct*)pack->pBuffer;
+		strcpy(skp->adminname, c->GetName());
+		strcpy(skp->name, name);
+		skp->adminrank = c->Admin();
+		worldserver.SendPacket(pack);
+		safe_delete(pack);
+	}
+
+	if (client) {
+		if (client->Admin() <= c->Admin()) {
+			client->Message_StringID(Chat::Red, StringID::BEEN_KICKED);
+			client->WorldKick();
+			c->Message(Chat::White, 
+				fmt::format(
+				"{} has been kicked from the server.",
+				name
+				).c_str()
+			);
 		}
 	}
 }
